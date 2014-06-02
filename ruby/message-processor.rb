fa2619b7dbc0222b9f0c47faa4e652a3865629e5
navaneethkn <navaneethkn@gmail.com>
2014-06-02 12:33:57 +0530

    First version of ruby runner

---
+++ ruby/message-processor.rb
@@ -0,0 +1,88 @@
+require_relative 'messages.pb'
+require_relative 'executor'
+require 'tempfile'
+
+EXECUTION_STARTING_REQUEST=0
+EXECUTE_STEP_REQUEST=1
+EXECUTE_STEP_RESPONSE=2
+EXECUTION_ENDING=3
+STEP_VALIDATE_REQUEST = 4
+STEP_VALIDATE_RESPONSE = 5
+
+class ExecuteStepProcessor
+	
+	def process(message)
+		step_text = message.executeStepRequest.stepText
+		args = message.executeStepRequest.args
+		begin
+			execute_step step_text, args
+	 	rescue Exception => e
+	 		return handle_step_failure message, e
+	 	end
+	 	handle_step_pass message
+	end
+
+	def handle_step_pass(message)
+		execution_step_response = Main::ExecuteStepResponse.new(:passed => true)
+		Main::Message.new(:messageType => EXECUTE_STEP_RESPONSE, :messageId => message.messageId, :executeStepResponse => execution_step_response) 
+	end
+
+	def handle_step_failure(message, exception)
+		execution_step_response = Main::ExecuteStepResponse.new(:passed => false,
+																:recoverableError => false,
+																:errorMessage => exception.message,
+																:stackTrace => exception.backtrace.join("\n"),
+																:screenShot => screenshot_bytes)
+		Main::Message.new(:messageType => EXECUTE_STEP_RESPONSE, :messageId => message.messageId, :executeStepResponse => execution_step_response) 
+	end
+
+	def screenshot_bytes
+		file = File.open("#{Dir.tmpdir}/screenshot.png", "w+")
+		`screencapture #{file.path}`
+		file_content = file.read
+		File.delete file
+		return file_content	
+	end
+end
+
+class ExecutionStartProcessor
+	def process(message)
+		return message
+	end
+end
+
+class ExecutionEndProcessor
+	def process(message)
+		return message
+	end
+end
+
+class StepValidationProcessor
+	def process(message)
+		step_text = message.stepValidateRequest.stepText
+		step_validate_response = Main::StepValidateResponse.new(:isValid => is_valid_step(step_text))
+		Main::Message.new(:messageType => STEP_VALIDATE_RESPONSE, :messageId => message.messageId, :stepValidateResponse => step_validate_response)
+	end
+end
+
+
+
+class MessageProcessor
+
+	@processors = Hash.new
+	@processors[EXECUTION_STARTING_REQUEST] = ExecutionStartProcessor.new
+	@processors[EXECUTE_STEP_REQUEST] = ExecuteStepProcessor.new
+	@processors[EXECUTION_ENDING] = ExecutionEndProcessor.new
+	@processors[STEP_VALIDATE_REQUEST] = StepValidationProcessor.new
+	
+	def self.is_valid_message(message)
+		return @processors.has_key? message.messageType	
+	end
+
+	def self.process_message(message)
+		@processors[message.messageType].process message
+	end
+
+end
+
+
---
+++ ruby/ruby-runtime.rb
@@ -0,0 +1,45 @@
+require 'socket'
+require 'protocol_buffers'
+
+require_relative 'messages.pb'
+require_relative 'executor'
+require_relative 'message-processor'
+
+HOST_NAME = 'localhost'
+PORT = 8888
+DEFAULT_IMPLEMENTATIONS_DIR_PATH = "#{Dir.pwd}/ruby/steps_implementation"
+
+def dispatch_messages(socket)
+	while (!socket.eof?)
+	  len = message_length(socket)
+	  data = socket.read len
+	  message = Main::Message.parse(data)
+	  handle_message(socket, message)
+	end
+end
+
+def handle_message(socket, message)
+	if (!MessageProcessor.is_valid_message(message)) 
+		puts "Invalid message received"	
+	else
+		response = MessageProcessor.process_message message
+		write_message(socket, response)
+	end
+end
+	
+def message_length(socket)
+	ProtocolBuffers::Varint.decode socket	
+end
+
+def write_message(socket, message)
+	serialized_message = message.to_s
+	size = serialized_message.bytesize
+	ProtocolBuffers::Varint.encode(socket, size)
+	socket.write serialized_message
+end
+
+
+socket = TCPSocket.open(HOST_NAME, PORT)
+load_steps(DEFAULT_IMPLEMENTATIONS_DIR_PATH)
+dispatch_messages(socket)
+
