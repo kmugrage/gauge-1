f68468b2220716ddf4c3423fa8687ef8f037bf24
vishnu karthik <karthikl@thoughtworks.com>
2014-06-09 19:13:49 +0530

    Fixing protobuf generation issues

--- genproto.sh
+++ genproto.sh
@@ -1,5 +1,5 @@
-PATH=$PATH:$GOPATH/bin protoc --go_out=src/ messages.proto
-PATH=$PATH:$GOPATH/bin protoc --go_out=src/ api.proto
-protoc --java_out=../gauge-java/src/main/java/ messages.proto 
-protoc --java_out=../gauge-java/src/main/java/ api.proto 
+PATH=$PATH:$GOPATH/bin protoc --go_out=gauge/ gauge/messages.proto
+PATH=$PATH:$GOPATH/bin protoc --go_out=gauge/ gauge/api.proto
+protoc --java_out=gauge-java/src/main/java/ gauge/messages.proto
+protoc --java_out=gauge-java/src/main/java/ gauge/api.proto
 #ruby-protoc -o ruby messages.proto 
