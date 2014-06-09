a5458111235671f64d4bdb31e027fdc2c8819657
prateekbaheti <prateekb@thoughtworks.com>
2014-06-09 14:16:05 +0530

    Adding execution of hooks

--- gauge-ruby/lib/executor.rb
+++ gauge-ruby/lib/executor.rb
@@ -1,10 +1,51 @@
 $steps_map = Hash.new
+$before_suite_hooks = []
+$after_suite_hooks = []
+$before_spec_hooks = []
+$after_spec_hooks = []
+$before_scenario_hooks = []
+$after_scenario_hooks = []
+$before_step_hooks = []
+$after_step_hooks = []
 
 def step(text, &block)
   $steps_map[text] = block;
 end
 
-def load_steps steps_implementation_dir
+def before_step(&block)
+  $before_step_hooks.push block;
+end
+
+def after_step(&block)
+  $after_step_hooks.push block;
+end
+
+def before_spec(&block)
+  $before_spec_hooks.push block;
+end
+
+def after_spec(&block)
+  $after_spec_hooks.push block;
+end
+
+def before_scenario(&block)
+  $before_scenario_hooks.push block;
+end
+
+def after_scenario(&block)
+  $after_scenario_hooks.push block;
+end
+
+def before_suite(&block)
+  $before_suite_hooks.push block;
+end
+
+def after_suite(&block)
+  $after_suite_hooks.push block;
+end
+
+
+def load_steps(steps_implementation_dir)
   Dir["#{steps_implementation_dir}/**/*.rb"].each { |x| require x }
 end
 
@@ -19,4 +60,11 @@ def execute_step(step, args)
   else
     block.call(args)
   end
+end
+
+def execute_hooks(hooks, currentExecutionInfo)
+  hooks.each do |hook|
+    hook.call(currentExecutionInfo)
+  end
+  return nil
 end
--- gauge-ruby/lib/message-processor.rb
+++ gauge-ruby/lib/message-processor.rb
@@ -3,106 +3,130 @@ require_relative 'executor'
 require 'tempfile'
 
 
-class ExecuteStepProcessor
+def process_execute_step_request(message)
+  step_text = message.executeStepRequest.parsedStepText
+  arguments = message.executeStepRequest.args
+  args = create_arg_values arguments
+  begin
+    execute_step step_text, args
+  rescue Exception => e
+    return handle_failure message, e
+  end
+  handle_pass message
+end
 
-  def process(message)
-    step_text = message.executeStepRequest.parsedStepText
-    arguments = message.executeStepRequest.args
-    args = create_arg_values arguments
-    begin
-      execute_step step_text, args
-    rescue Exception => e
-      return handle_step_failure message, e
+def create_arg_values arguments
+  args = []
+  arguments.each do |argument|
+    if (argument.type == "table")
+      args.push argument.table
+    else
+      args.push argument.value
     end
-    handle_step_pass message
   end
-
-  def create_arg_values arguments
-    args = []
-    arguments.each do |argument|
-      if (argument.type = "table")
-        args.push argument.table
-      else
-        args.push argument.value
-      end
-    end
-    return args
-  end
-
-  def handle_step_pass(message)
-    execution_status_response = Main::ExecutionStatusResponse.new(:executionStatus => Main::ExecutionStatus.new(:passed => true))
-    Main::Message.new(:messageType => Main::Message::MessageType::ExecutionStatusResponse, :messageId => message.messageId, :executionStatusResponse => execution_status_response)
-  end
-
-  def handle_step_failure(message, exception)
-    execution_status_response = Main::ExecutionStatusResponse.new(:executionStatus => Main::ExecutionStatus.new(:passed => false,
-                                                                                                                :recoverableError => false,
-                                                                                                                :errorMessage => exception.message,
-                                                                                                                :stackTrace => exception.backtrace.join("\n"),
-                                                                                                                :screenShot => screenshot_bytes))
-    Main::Message.new(:messageType => Main::Message::MessageType::ExecutionStatusResponse, :messageId => message.messageId, :executionStatusResponse => execution_status_response)
-  end
-
-  def screenshot_bytes
-    file = File.open("#{Dir.tmpdir}/screenshot.png", "w+")
-    `screencapture #{file.path}`
-    file_content = file.read
-    File.delete file
-    return file_content
-  end
+  return args
 end
 
-class ExecutionStartProcessor
-  def process(message)
-    execution_status_response = Main::ExecutionStatusResponse.new(:executionStatus => Main::ExecutionStatus.new(:passed => true))
-    Main::Message.new(:messageType => Main::Message::MessageType::ExecutionStatusResponse, :messageId => message.messageId, :executionStatusResponse => execution_status_response)
-  end
+def process_execution_start_request(message)
+  handle_hooks_execution($before_suite_hooks, message, message.executionStartingRequest.currentExecutionInfo)
 end
 
-class StepValidationProcessor
-  def process(message)
-    step_validate_request = message.stepValidateRequest
-    step_validate_response = Main::StepValidateResponse.new(:isValid => is_valid_step(step_validate_request.stepText))
-    Main::Message.new(:messageType => Main::Message::MessageType::StepValidateResponse, :messageId => message.messageId, :stepValidateResponse => step_validate_response)
-  end
+def process_execution_end_request(message)
+  handle_hooks_execution($after_suite_hooks, message, message.executionEndingRequest.currentExecutionInfo)
 end
 
-class StepNamesProcessor
-  def process(message)
-    step_names_response = Main::StepNamesResponse.new(:steps => $steps_map.keys)
-    Main::Message.new(:messageType => Main::Message::MessageType::StepNamesResponse, :messageId => message.messageId, :stepNamesResponse => step_names_response)
-  end
+def process_spec_execution_start_request(message)
+  handle_hooks_execution($before_spec_hooks, message, message.specExecutionStartingRequest.currentExecutionInfo)
 end
 
-class KillProcessProcessor
-  def process(message)
-    return message
-  end
+def process_spec_execution_end_request(message)
+  handle_hooks_execution($after_spec_hooks, message, message.specExecutionEndingRequest.currentExecutionInfo)
+end
+
+def process_scenario_execution_start_request(message)
+  handle_hooks_execution($before_scenario_hooks, message, message.scenarioExecutionStartingRequest.currentExecutionInfo)
+end
+
+def process_scenario_execution_end_request(message)
+  handle_hooks_execution($after_scenario_hooks, message, message.scenarioExecutionEndingRequest.currentExecutionInfo)
+end
+
+def process_step_execution_start_request(message)
+  handle_hooks_execution($before_step_hooks, message, message.stepExecutionStartingRequest.currentExecutionInfo)
+end
+
+def process_step_execution_end_request(message)
+  handle_hooks_execution($after_step_hooks, message, message.stepExecutionEndingRequest.currentExecutionInfo)
+end
+
+def process_step_validation_request(message)
+  step_validate_request = message.stepValidateRequest
+  step_validate_response = Main::StepValidateResponse.new(:isValid => is_valid_step(step_validate_request.stepText))
+  Main::Message.new(:messageType => Main::Message::MessageType::StepValidateResponse, :messageId => message.messageId, :stepValidateResponse => step_validate_response)
+end
+
+def process_step_names_request(message)
+  step_names_response = Main::StepNamesResponse.new(:steps => $steps_map.keys)
+  Main::Message.new(:messageType => Main::Message::MessageType::StepNamesResponse, :messageId => message.messageId, :stepNamesResponse => step_names_response)
+end
+
+def process_kill_processor_request(message)
+  return message
 end
 
 class MessageProcessor
   @processors = Hash.new
-  @processors[Main::Message::MessageType::StepValidateRequest] = StepValidationProcessor.new
-  @processors[Main::Message::MessageType::ExecutionStarting] = ExecutionStartProcessor.new
-  @processors[Main::Message::MessageType::SpecExecutionStarting] = ExecutionStartProcessor.new
-  @processors[Main::Message::MessageType::SpecExecutionEnding] = ExecutionStartProcessor.new
-  @processors[Main::Message::MessageType::ScenarioExecutionStarting] = ExecutionStartProcessor.new
-  @processors[Main::Message::MessageType::ScenarioExecutionEnding] = ExecutionStartProcessor.new
-  @processors[Main::Message::MessageType::StepExecutionStarting] = ExecutionStartProcessor.new
-  @processors[Main::Message::MessageType::StepExecutionEnding] = ExecutionStartProcessor.new
-  @processors[Main::Message::MessageType::ExecuteStep] = ExecuteStepProcessor.new
-  @processors[Main::Message::MessageType::ExecutionEnding] = ExecutionStartProcessor.new
-  @processors[Main::Message::MessageType::StepNamesRequest] = StepNamesProcessor.new
-  @processors[Main::Message::MessageType::KillProcessRequest] = KillProcessProcessor.new
+  @processors[Main::Message::MessageType::StepValidateRequest] = method(:process_step_validation_request)
+  @processors[Main::Message::MessageType::ExecutionStarting] = method(:process_execution_start_request)
+  @processors[Main::Message::MessageType::ExecutionEnding] = method(:process_execution_end_request)
+  @processors[Main::Message::MessageType::SpecExecutionStarting] = method(:process_spec_execution_start_request)
+  @processors[Main::Message::MessageType::SpecExecutionEnding] = method(:process_spec_execution_end_request)
+  @processors[Main::Message::MessageType::ScenarioExecutionStarting] = method(:process_scenario_execution_start_request)
+  @processors[Main::Message::MessageType::ScenarioExecutionEnding] = method(:process_scenario_execution_end_request)
+  @processors[Main::Message::MessageType::StepExecutionStarting] = method(:process_step_execution_start_request)
+  @processors[Main::Message::MessageType::StepExecutionEnding] = method(:process_step_execution_end_request)
+  @processors[Main::Message::MessageType::ExecuteStep] = method(:process_execute_step_request)
+  @processors[Main::Message::MessageType::StepNamesRequest] = method(:process_step_names_request)
+  @processors[Main::Message::MessageType::KillProcessRequest] = method(:process_kill_processor_request)
 
   def self.is_valid_message(message)
     return @processors.has_key? message.messageType
   end
 
   def self.process_message(message)
-    @processors[message.messageType].process message
+    @processors[message.messageType].call message
   end
 
 end
 
+def handle_hooks_execution(hooks, message, currentExecutionInfo)
+  execution_error = execute_hooks(hooks, currentExecutionInfo)
+  if execution_error == nil
+    return handle_pass message
+  else
+    return handle_failure message, execution_error
+  end
+end
+
+def handle_pass(message)
+  execution_status_response = Main::ExecutionStatusResponse.new(:executionStatus => Main::ExecutionStatus.new(:passed => true))
+  Main::Message.new(:messageType => Main::Message::MessageType::ExecutionStatusResponse, :messageId => message.messageId, :executionStatusResponse => execution_status_response)
+end
+
+def handle_failure(message, exception)
+  execution_status_response = Main::ExecutionStatusResponse.new(:executionStatus => Main::ExecutionStatus.new(:passed => false,
+                                                                                                              :recoverableError => false,
+                                                                                                              :errorMessage => exception.message,
+                                                                                                              :stackTrace => exception.backtrace.join("\n"),
+                                                                                                              :screenShot => screenshot_bytes))
+  Main::Message.new(:messageType => Main::Message::MessageType::ExecutionStatusResponse, :messageId => message.messageId, :executionStatusResponse => execution_status_response)
+end
+
+def screenshot_bytes
+  file = File.open("#{Dir.tmpdir}/screenshot.png", "w+")
+  `screencapture #{file.path}`
+  file_content = file.read
+  File.delete file
+  return file_content
+end
 
