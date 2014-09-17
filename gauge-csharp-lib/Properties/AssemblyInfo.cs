28a9837446c3e263bed2498b6cfa51bce709bc77
vishnu <vishnucarbon@gmail.com>
2014-09-17 15:01:27 +0530

    Adding api functionality to csharp

--- gauge-csharp-lib/Properties/AssemblyInfo.cs
+++ gauge-csharp-lib/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("gauge-csharp")]
+[assembly: AssemblyTitle("gauge-csharp-lib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("gauge-csharp")]
+[assembly: AssemblyProduct("gauge-csharp-lib")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b80cc90b-dd04-445b-825e-51a42f3cadaf")]
+[assembly: Guid("21677428-1b2a-4a9b-9865-f72209fb8f1c")]
 
 // Version information for an assembly consists of the following four values:
 //
